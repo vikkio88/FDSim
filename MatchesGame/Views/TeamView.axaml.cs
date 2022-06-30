63ca6991c34956d30ec28906ba603c061996015e
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2022-06-30 19:24:07 +0100

    small prototype

---
+++ MatchesGame/Views/TeamView.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MatchesGame.Views
+{
+    public partial class TeamView : UserControl
+    {
+        public TeamView()
+        {
+            InitializeComponent();
+        }
+    }
+}
