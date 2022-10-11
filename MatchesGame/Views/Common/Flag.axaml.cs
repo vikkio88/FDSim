8b8b821b9be90857c2ca2d1c1f6db7aef787ef06
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2022-10-11 18:34:52 +0100

    fixed issue

--- MatchesGame/Views/Common/Flag.axaml.cs
+++ MatchesGame/Views/Common/Flag.axaml.cs
@@ -1,10 +1,11 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.Primitives;
 
 namespace MatchesGame.Views.Common;
 public class Flag : TemplatedControl
 {
-    public static readonly StyledProperty<string> IsoProperty = AvaloniaProperty.Register<Stars, string>(nameof(IsoProperty));
+    public static readonly StyledProperty<string> IsoProperty = AvaloniaProperty.Register<Flag, string>(nameof(Iso));
     public string Iso
     {
         get => GetValue(IsoProperty);
