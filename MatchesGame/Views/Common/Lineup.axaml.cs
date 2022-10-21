73498e02c77529b4829d9fa08d2c255690b482a7
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2022-10-21 19:43:05 +0100

    finished lineup component

--- MatchesGame/Views/Common/Lineup.axaml.cs
+++ MatchesGame/Views/Common/Lineup.axaml.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reactive.Linq;
 using Avalonia;
 using Avalonia.Controls.Primitives;
 using FDSim.Models.People;
 using FDSim.Models.Enums;
 using FLineup = FDSim.Models.Game.Team.Lineup;
+using System.Reactive;
+using ReactiveUI;
+using System.Windows.Input;
 
 namespace MatchesGame.Views.Common;
 public class Lineup : TemplatedControl
@@ -22,9 +26,22 @@ public class Lineup : TemplatedControl
     }
 
     public Player? GK { get; set; } = null;
-    public List<Player> DF { get; set; } = new();
-    public List<Player> MF { get; set; } = new();
-    public List<Player> ST { get; set; } = new();
+    public List<LineupSpot> DF { get; set; } = new();
+    public List<LineupSpot> MF { get; set; } = new();
+    public List<LineupSpot> ST { get; set; } = new();
+
+    public static readonly DirectProperty<Lineup, ICommand?> OnPlayerClickProperty =
+           AvaloniaProperty.RegisterDirect<Lineup, ICommand?>(nameof(OnPlayerClick),
+               l => l.OnPlayerClick, (l, command) => l.OnPlayerClick = command);
+
+    private ICommand? _onPlayerClick;
+    public ICommand? OnPlayerClick
+    {
+        get => _onPlayerClick;
+        set => SetAndRaise(OnPlayerClickProperty, ref _onPlayerClick, value);
+    }
+
+    public ReactiveUI.ReactiveCommand<string, Unit> OnPlayerClickInternal { get; set; }
 
     static Lineup()
     {
@@ -34,9 +51,30 @@ public class Lineup : TemplatedControl
 
     public void OnValueChanged()
     {
+
+
+        var teamId = Value.TeamId;
+        OnPlayerClickInternal = ReactiveCommand.Create((string playerId) =>
+        {
+            OnPlayerClick?.Execute($"{teamId}.{playerId}");
+        });
+
         GK = Value.Starters.Find(p => p.Role == Role.Goalkeeper);
-        DF = Value.Starters.FindAll(p => p.Role == Role.Defender);
-        MF = Value.Starters.FindAll(p => p.Role == Role.Midfielder);
-        ST = Value.Starters.FindAll(p => p.Role == Role.Striker);
+        DF = Value.Starters.FindAll(p => p.Role == Role.Defender).Select(p => new LineupSpot(p, OnPlayerClickInternal)).ToList();
+        MF = Value.Starters.FindAll(p => p.Role == Role.Midfielder).Select(p => new LineupSpot(p, OnPlayerClickInternal)).ToList();
+        ST = Value.Starters.FindAll(p => p.Role == Role.Striker).Select(p => new LineupSpot(p, OnPlayerClickInternal)).ToList();
     }
+}
+
+public class LineupSpot
+{
+    public Player Player { get; }
+    public ICommand OnClick { get; }
+
+    public LineupSpot(Player player, ICommand onClick)
+    {
+        Player = player;
+        OnClick = onClick;
+    }
+
 }
