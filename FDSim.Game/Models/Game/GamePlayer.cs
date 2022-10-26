335cef31776318fe0febee9bca44086653ec3490
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2022-10-26 20:25:32 +0100

    started working on GamePlayer object

---
+++ FDSim.Game/Models/Game/GamePlayer.cs
@@ -0,0 +1,21 @@
+using System;
+using FDSim.Models.Common;
+using FDSim.Models.Enums;
+
+namespace FDSim.Game.Models.Game;
+public class GamePlayer
+{
+    public string Name { get; init; }
+    public string Surname { get; init; }
+    public Nationality Nationality { get; init; }
+    public int Age { get; init; }
+    public string PrintName { get => $"{Name} {Surname}"; }
+    public string ShortName { get => $"{Name.Substring(0, 1)}. {Surname}"; }
+    public Perc Reputation { get; init; } = new(20);
+    public GamePlayer(string FullName, int Age)
+    {
+        var names = FullName.Split(' ');
+        Surname = names[names.Length - 1];
+        Name = string.Join(" ", new ArraySegment<string>(names, 0, names.Length - 1));
+    }
+}
