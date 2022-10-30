22e04de15d946adb3b0165dfcec2f9e2384e98da
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2022-10-30 15:20:54 +0000

    upgrade to avalonia 11

--- FDSim.Game/ViewModels/BaseRxViewModel.cs
+++ FDSim.Game/ViewModels/BaseRxViewModel.cs
@@ -10,7 +10,7 @@ abstract public class BaseRxViewModel : ReactiveObject, IRoutableViewModel
     virtual public string? UrlPathSegment { get; }
 
     public IScreen HostScreen { get; }
-    public ReactiveCommand<Unit, Unit>? Back { get; set; } = null;
+    public ReactiveCommand<Unit, IRoutableViewModel?>? Back { get; set; } = null;
     public ReactiveCommand<Unit, IRoutableViewModel>? GoToDashboard { get; set; } = null;
 
 
